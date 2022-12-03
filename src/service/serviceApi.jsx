ca12652def82d2d363edb9001f5c6661555f4b0e
Dmytro Afanasiev <afanasiev.dimon@outlook.com>
2022-12-03 16:59:12 +0100

    fix bug

--- src/service/serviceApi.jsx
+++ src/service/serviceApi.jsx
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const getAllTransactions = async (page = 1, limit = 14) => {
   const { data } = await axios.get(
-    `https://ethereum-back-test.herokuapp.com/api/transactions?page=${page}&limit=${limit}`
+    `https://ethereum-back-test.onrender.com/api/transactions?page=${page}&limit=${limit}`
   );
   return data;
 };
 
 const getTransactionsBySearch = async (dataForm, page = 1, limit = 14) => {
   const { data } = await axios.get(
-    `https://ethereum-back-test.herokuapp.com/api/transactions/search?page=${page}&limit=${limit}&search=${dataForm.search}&type=${dataForm.type}`
+    `https://ethereum-back-test.onrender.com/api/transactions/search?page=${page}&limit=${limit}&search=${dataForm.search}&type=${dataForm.type}`
   );
   return data;
 };
